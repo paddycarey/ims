ad3a2effa29df9113618361c89fd71844b0a81e2
Patrick Carey <patrick@rehabstudio.com>
2015-04-09 19:08:01 +0100

    move cli args parsing code into config module

---
+++ config/docopt.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"github.com/docopt/docopt-go"
+)
+
+var usage string = `ims.
+
+Usage:
+  ims [--storage=<src>] [--cache=<cch>] [--address=<address>]
+      [--log-level=<level>] [--no-optimization]
+  ims -h | --help
+  ims --version
+
+Options:
+  -h --help                      Show this screen.
+  --version                      Show version.
+  --storage=<src>                Storage backend                    [default: ./].
+  --cache=<cch>                  Cache backend                      [default: ./.cache].
+  --address=<address>            Address that ims should bind to    [default: :5995].
+  --log-level=<level>            Log level (debug/info/warn/error)  [default: info].
+  --no-optimization              Disables image optimization.`
+
+type Config struct {
+	Storage        string
+	Cache          string
+	Address        string
+	LogLevel       string
+	NoOptimization bool
+}
+
+func ParseCLIConfig() *Config {
+	// parse command line args, exiting if required
+	arguments, _ := docopt.Parse(usage, nil, true, "ims 0.1", false)
+	return &Config{
+		Storage:        arguments["--storage"].(string),
+		Cache:          arguments["--cache"].(string),
+		Address:        arguments["--address"].(string),
+		LogLevel:       arguments["--log-level"].(string),
+		NoOptimization: arguments["--no-optimization"].(bool),
+	}
+}
--- main.go
+++ main.go
@@ -5,30 +5,13 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/negroni"
-	"github.com/docopt/docopt-go"
 	"github.com/meatballhat/negroni-logrus"
 
 	"github.com/paddycarey/ims/cache"
+	"github.com/paddycarey/ims/config"
 	"github.com/paddycarey/ims/server"
 	"github.com/paddycarey/ims/storage"
 )
-
-var usage string = `ims.
-
-Usage:
-  ims [--storage=<src>] [--storage-credentials=<creds>] [--cache=<cch>]
-      [--address=<address>] [--log-level=<level>] [--no-optimization]
-  ims -h | --help
-  ims --version
-
-Options:
-  -h --help                      Show this screen.
-  --version                      Show version.
-  --storage=<src>                Storage backend                    [default: ./].
-  --cache=<cch>                  Cache backend                      [default: ./.cache].
-  --address=<address>            Address that ims should bind to    [default: :5995].
-  --log-level=<level>            Log level (debug/info/warn/error)  [default: info].
-  --no-optimization              Disables image optimization.`
 
 // exitOnError checks that an error is not nil. If the passed value is an
 // error, it is logged and the program exits with an error code of 1
@@ -44,25 +27,25 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// parse command line args, exiting if required
-	arguments, _ := docopt.Parse(usage, nil, true, "ims 0.1", false)
+	args := config.ParseCLIConfig()
 
 	// configure global logger instance
-	logLevel, err := logrus.ParseLevel(arguments["--log-level"].(string))
+	logLevel, err := logrus.ParseLevel(args.LogLevel)
 	exitOnError(err, "Unable to initialise logger")
 	logrus.SetLevel(logLevel)
 
 	// load cache backend
-	c, err := cache.LoadBackend(arguments["--cache"].(string))
+	c, err := cache.LoadBackend(args.Cache)
 	exitOnError(err, "Unable to load cache backend")
 
 	// load storage backend
-	s, err := storage.LoadBackend(arguments["--storage"].(string))
+	s, err := storage.LoadBackend(args.Storage)
 	exitOnError(err, "Unable to load storage backend")
 
 	// run application server
 	n := negroni.New()
 	n.Use(negronilogrus.NewMiddleware())
 	n.Use(negroni.NewRecovery())
-	n.UseHandler(&server.Server{Cache: c, Storage: s, NoOpts: arguments["--no-optimization"].(bool)})
-	n.Run(arguments["--address"].(string))
+	n.UseHandler(&server.Server{Cache: c, Storage: s, NoOpts: args.NoOptimization})
+	n.Run(args.Address)
 }
